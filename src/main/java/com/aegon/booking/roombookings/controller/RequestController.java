bdf5ff2ce59699605b49416be15ff7a0d351be45
Nareshkumar <Nareshkumar@Naresh-book>
2017-06-23 00:52:58 +0530

    RemovedDuplicateFiles

--- src/main/java/com/aegon/booking/roombookings/controller/RequestController.java
+++ src/main/java/com/aegon/booking/roombookings/controller/RequestController.java
@@ -16,20 +16,8 @@ import com.aegon.booking.roombookings.service.BookingService;
 @RequestMapping("/lochside")
 public class RequestController {
 
-	/*@Autowired
-	BookingService bookingService = new BookingServiceImpl();*/
-	
 	@Autowired
 	BookingService bookingService;
-	
-	/*@Autowired
-	BookingEntityRepository bookingEntityRepository;*/
-	
-	/*@RequestMapping("/rooms/{roomId}")
-	public List<Booking> getBoookingByRoomId(@PathVariable("roomId") int roomId)
-	{
-		return bookingService.getBoookingByRoomId(roomId);
-	}*/
 	
 	@RequestMapping(value = "/booking", method = RequestMethod.POST)
 	public BookingEntity newBooking(@RequestBody BookingEntity bookingEntity)
@@ -50,34 +38,15 @@ public class RequestController {
 		return bookingService.getBoookingByCustId(custId);
 	}
 	
-	@RequestMapping("/availability/from/{fromDate}/to/{toDate}")
-	public List<BookingEntity> getRoomAvailability(@PathVariable("fromDate") String fromDate, @PathVariable("toDate") int toDate)
-	{
-		return null;
-	}
-	
 	@RequestMapping(value = "/booking/{bookingId}", method = RequestMethod.PUT)
 	public BookingEntity updateBooking(@PathVariable("bookingId") Long bookingId, @RequestBody BookingEntity bookingEntity)
 	{
 		return bookingService.updateBooking(bookingId, bookingEntity);
 	}
-	
-	/*@RequestMapping(value = "/booking/", method = RequestMethod.POST)
-	public Booking newBooking(@RequestBody Booking booking)
+
+	@RequestMapping("/availability/from/{fromDate}/to/{toDate}")
+	public List<BookingEntity> getRoomAvailability(@PathVariable("fromDate") String fromDate, @PathVariable("toDate") int toDate)
 	{
-		
-		bookingService.newBooking(booking);
-		return booking;
-	}*/
-	
-	
-	/*@RequestMapping(value = "/booking/{bookingId}", method = RequestMethod.PUT)
-	public Booking updateBooking(@PathVariable("bookingId") Long bookingId, @RequestBody Booking booking)
-	{
-		Booking updatedBooking = bookingService.updateBooking(bookingId, booking);
-		return updatedBooking;
 		return null;
-	}*/
-	
-	
+	}
 }
